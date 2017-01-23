661f56b0cd9f3d20d8428f0f2951bef1e7d05f97
Antoine GIRARD <sapk@sapk.fr>
2017-01-23 18:16:29 +0100

    First parsing of AVI

--- modules/collectors/anssi/anssi.go
+++ modules/collectors/anssi/anssi.go
@@ -16,7 +16,7 @@ const (
 	aviURLFormat       = `http://www.cert.ssi.gouv.fr/site/%s/index.html`
 	aviDirectURLFormat = `http://www.cert.ssi.gouv.fr/site/%s/%s.html`
 	aviRegex           = `^CERT(FR|A)-%d-AVI-[0-9]+$`
-	minYear            = 2000
+	minYear            = 2017
 )
 
 var maxYear = 2000 //Updated at run time
@@ -52,7 +52,12 @@ func (m *ModuleANSSI) IsAvailable() bool {
 
 //Collect collect and parse data to put in database
 func (m *ModuleANSSI) Collect() error {
-	log.WithFields(log.Fields{}).Debugf("Start collect for : '%s'", id)
+	log.WithFields(log.Fields{
+		"minYear": minYear,
+		"maxYear": maxYear,
+	}).Debugf("Start collect for : '%s'", id)
+
+	parsedAVI := 0
 
 	for y := minYear; y <= maxYear; y++ {
 		url := fmt.Sprintf(listURLFormat, y)
@@ -69,19 +74,78 @@ func (m *ModuleANSSI) Collect() error {
 				"yearURL": url,
 			}).Warnf("Faild to get list : %v", err)
 		} else {
-			// Find the review items
+			// Find CVE link
 			doc.Find(".corps a.mg").Each(func(i int, s *goquery.Selection) {
 				title := s.Text()
 				if validAVI.MatchString(title) {
 					log.Debugf("CVE found %d: %s", i, title)
+					parseAVI(title) //TODO get returned object and store it in DB
+					parsedAVI++
 				} else {
 					log.Debugf("Skipping id : %s", title)
 				}
 			})
 		}
 	}
+
+	log.WithFields(log.Fields{
+		"parsedAVI": parsedAVI,
+	}).Debugf("Ended collect for : '%s'", id)
 	return nil
 	//return fmt.Errorf("Module '%s' does not implement Collect().", id) //TODO
+}
+
+func parseAVI(id string) error {
+	url := fmt.Sprintf(aviDirectURLFormat, id, id)
+	log.WithFields(log.Fields{
+		"id":  id,
+		"url": url,
+	}).Debugf("Getting CVE (%s) from : '%s'", id, url)
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"id":  id,
+			"url": url,
+		}).Warnf("Faild to get CVE : %v", err)
+	} else {
+		// Find the values
+		title := doc.Find("head>title").Text()
+
+		headers := make(map[string]string)
+		doc.Find("td.corps > div[align='center'] > table  div[align='center'] > table tr[valign='baseline']").Each(func(i int, s *goquery.Selection) {
+			title := s.Find("td:nth-child(1)").Text()
+			value := s.Find("td:nth-child(2)").Text()
+			headers[title] = value
+		})
+
+		contents := make(map[string]string)
+		doc.Find("td.corps > ul").Each(func(i int, s *goquery.Selection) {
+			switch i {
+			case 0:
+				contents["Risques"] = s.Text()
+			case 1:
+				contents["Documentation"] = s.Text()
+			}
+		})
+		doc.Find("td.corps > p").Each(func(i int, s *goquery.Selection) {
+			switch i {
+			case 5:
+				contents["Systèmes"] = s.Text()
+			case 6:
+				contents["Résumé"] = s.Text()
+			}
+		})
+
+		log.WithFields(log.Fields{
+			"id":       id,
+			"url":      url,
+			"title":    title,
+			"headers":  headers,
+			"contents": contents,
+		}).Debug("CVE parsed")
+		//TODO return object (to defin in model)
+	}
+	return nil
 }
 
 //List display known CVE stored by this module in DB
